a126b966fdc3f54c2430b19df988be1059f7f991
u92314lp <laurent.pool@student.manchester.ac.uk>
2021-03-17 06:30:27 -0400

    Added syscall tester to test write to std out

--- usr/tests/hello.c
+++ usr/tests/hello.c
@@ -16,6 +16,58 @@
 
 #define N	255
 
+/* For the write_asm function, the C variables used inside the asm()
+   construct need to have global scope. */
+volatile size_t fd_g = 0;
+volatile char *buf_g;
+volatile size_t len_g;
+volatile ssize_t ret_g;
+
+
+static ssize_t write_asm(int fd, char *buf)
+{
+	/* Take arguments and assign them to globally declared variables
+	   so we can use them in the asm() construct */
+	fd_g = fd;
+	buf_g = buf;
+	len_g = strlen(buf);
+
+	asm volatile("mfence":::"memory");
+	asm volatile  (	"mov	$1, %%rax\n\t"
+			"mov	fd_g, %%rdi\n\t"
+			"mov	buf_g, %%rsi\n\t"
+			"mov 	len_g, %%rdx\n\t"
+			"syscall\n\t"
+			"mov    %%rax, ret_g\n\t"
+			:
+			:
+			: "%rax", "%rdi", "%rsi", "%rdx"
+			);
+	
+	return ret_g;
+}
+
+static ssize_t printf_asm(char *buf)
+{
+	return write_asm(1, buf);
+}
+
+static void syscall_tester(void)
+{
+	ssize_t ret;
+
+	ret = printf_asm("Hello World from the assembler!!\n");
+	printf("Returned from first syscall\n");
+	printf("%zd values were printed\n\n", ret);
+
+	ret = printf_asm("Hello once again from the assembler!!\n");
+	printf("Returned from second syscall\n");
+	printf("%zd values were printed\n\n", ret);
+
+	//ret = write_asm(1, "sadf\n");
+	//printf_asm("jahskjdhfashf\n");
+}
+
 int main(int argc, char** argv)
 {
 	int i, random;
@@ -43,6 +95,8 @@ int main(int argc, char** argv)
 		fprintf(file, "Hello World!!!\n");
 		fclose(file);
 	} else fprintf(stderr, "Unable to open file /tmp/test.txt\n");
+
+	syscall_tester();
 
 	return 0;
 }
